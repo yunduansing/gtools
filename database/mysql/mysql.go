4e691060545d0c79913efbf3d53a2cea5163612c
xiangjun.lu <1044699395@qq.com>
2023-09-07 14:07:26 +0800

    优化logger、mysql

--- database/mysql/mysql.go
+++ database/mysql/mysql.go
@@ -47,12 +47,11 @@ func NewMySQLFromConfig(c *Config, opts ...Option) (*gorm.DB, error) {
 		Logger: logger.New(log.New(os.Stdout, "\r\n", log.LstdFlags), logger.Config{
 			LogLevel: logger.Info,
 		}),
+		PrepareStmt: true,
 	})
-	for _, opt := range opts {
-		opt()
-	}
+
 	if err != nil {
-		sysLog.Error("open mysql err:", err)
+		sysLog.Error("Init open mysql err:", err)
 		panic(err)
 	}
 	ic := idleConn
@@ -63,8 +62,14 @@ func NewMySQLFromConfig(c *Config, opts ...Option) (*gorm.DB, error) {
 	if c.MaxConn > 0 {
 		mc = c.MaxConn
 	}
-	db.Use(dbresolver.Register(dbresolver.Config{}).SetMaxIdleConns(ic).SetMaxOpenConns(mc).SetConnMaxIdleTime(time.Hour).SetConnMaxLifetime(24 * time.Hour))
-
+	err = db.Use(dbresolver.Register(dbresolver.Config{}).SetMaxIdleConns(ic).SetMaxOpenConns(mc).SetConnMaxIdleTime(time.Hour).SetConnMaxLifetime(24 * time.Hour))
+	if err != nil {
+		sysLog.Error("Register mysql plugin err:", err)
+		panic(err)
+	}
+	for _, opt := range opts {
+		opt()
+	}
 	return db, err
 }
 
@@ -73,46 +78,26 @@ func SetMultiDb(db *gorm.DB) {
 }
 
 // NewMySQLFromConnString 创建gorm mysql DB
-func NewMySQLFromConnString(ds string) (*gorm.DB, error) {
+func NewMySQLFromConnString(ds string, opts ...Option) (*gorm.DB, error) {
 	db, err := gorm.Open(mysql.Open(ds), &gorm.Config{
 		Logger: logger.New(log.New(os.Stdout, "", log.LstdFlags), logger.Config{
 			LogLevel: logger.Info,
 		}),
+		PrepareStmt: true,
 	})
 	if err != nil {
-		sysLog.Error("open mysql err:", err)
+		sysLog.Error("Init open mysql err:", err)
 		panic(err)
 	}
 	ic := idleConn
 	mc := maxConn
-	db.Use(dbresolver.Register(dbresolver.Config{}).SetMaxIdleConns(ic).SetMaxOpenConns(mc).SetConnMaxIdleTime(time.Hour).SetConnMaxLifetime(24 * time.Hour))
+	err = db.Use(dbresolver.Register(dbresolver.Config{}).SetMaxIdleConns(ic).SetMaxOpenConns(mc).SetConnMaxIdleTime(time.Hour).SetConnMaxLifetime(24 * time.Hour))
+	if err != nil {
+		sysLog.Error("Register mysql plugin err:", err)
+		panic(err)
+	}
+	for _, opt := range opts {
+		opt()
+	}
 	return db, nil
-}
-
-// NewMySQL 创建gorm mysql DB
-func NewMySQL(c *Config) (*gorm.DB, error) {
-	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", c.Username, c.Password, c.Host, c.Port, c.DbName)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		sysLog.Error("open mysql err:", err)
-		panic(err)
-	}
-	ic := idleConn
-	mc := maxConn
-	db.Use(dbresolver.Register(dbresolver.Config{}).SetMaxIdleConns(ic).SetMaxOpenConns(mc).SetConnMaxIdleTime(time.Hour).SetConnMaxLifetime(24 * time.Hour))
-	return db, err
-}
-
-// NewMySQLByDS 创建gorm mysql DB
-func NewMySQLByDS(ds string) (*gorm.DB, error) {
-	db, err := gorm.Open(mysql.Open(ds), &gorm.Config{})
-	if err != nil {
-		sysLog.Error("open mysql err:", err)
-		panic(err)
-	}
-	ic := idleConn
-	mc := maxConn
-	db.Use(dbresolver.Register(dbresolver.Config{}).SetMaxIdleConns(ic).SetMaxOpenConns(mc).SetConnMaxIdleTime(time.Hour).SetConnMaxLifetime(24 * time.Hour))
-	return db, err
 }
