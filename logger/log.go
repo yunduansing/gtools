e1cf028f0982ec0f8a28847efd718bf3e3bcac3e
Ian <yunduansing@qq.com>
2025-04-03 16:59:49 +0800

    优化log代码

--- logger/log.go
+++ logger/log.go
@@ -28,49 +28,56 @@ func init() {
 	}
 }
 
+// 初始化日志
 func InitLog(c Config) {
-	if len(c.FilePath) == 0 {
+	if c.FilePath == "" {
 		c.FilePath = "./logs"
 	}
-	switch c.LogType {
-	case LogTypeLogrus:
+	if c.LogType == LogTypeLogrus {
 		logger = newLogrusLog(c)
-	//case "zap":
-	//logger = newZapLog(c)
-	default:
+	} else {
 		logger = newZapLog(c)
 	}
 }
 
+// 绑定 Context
 func (l *Logger) WithContext(ctx context.Context) *Logger {
 	l.ctx = ctx
 	return l
 }
 
+// 追加 Key-Value 字段
 func (l *Logger) WithField(key string, val any) *Logger {
-	l.kv = append(l.kv, KeyPair{
-		Key: key,
-		Val: val,
-	})
+	l.kv = append(l.kv, KeyPair{Key: key, Val: val})
 	return l
 }
 
+// 获取 Logger 实例
 func GetLogger() *Logger {
 	if logger == nil {
-		once.Do(func() {
-			logger = newZapLog(Config{
-				Level:       "Info",
-				FilePath:    "./logs",
-				LogType:     "zap",
-				ServiceName: "",
-			})
-		})
+		once.Do(
+			func() {
+				logger = newZapLog(
+					Config{
+						Level:       "Info",
+						FilePath:    "./logs",
+						LogType:     "zap",
+						ServiceName: "",
+					},
+				)
+			},
+		)
 	}
 
-	return p.Get().(*Logger)
+	logInstance := p.Get().(*Logger)
+	logInstance.logger = logger
+	return logInstance
 }
 
-func (l *Logger) Info(ctx context.Context, v ...interface{}) {
+// 统一处理日志方法，减少重复代码
+func (l *Logger) logWithContext(
+	ctx context.Context, logFunc func(ctx context.Context, v ...interface{}), v ...interface{},
+) {
 	if len(l.kv) > 0 {
 		ctx = context.WithValue(ctx, "kv", l.kv)
 	}
@@ -78,97 +85,57 @@ func (l *Logger) Info(ctx context.Context, v ...interface{}) {
 		l.kv = nil
 		p.Put(l)
 	}()
-	l.logger.Info(ctx, v...)
+	logFunc(ctx, v...)
+}
+
+// 统一处理日志格式化方法
+func (l *Logger) logfWithContext(
+	ctx context.Context, logFunc func(ctx context.Context, format string, v ...interface{}), format string,
+	v ...interface{},
+) {
+	if len(l.kv) > 0 {
+		ctx = context.WithValue(ctx, "kv", l.kv)
+	}
+	defer func() {
+		l.kv = nil
+		p.Put(l)
+	}()
+	logFunc(ctx, format, v...)
+}
+
+// 具体日志方法调用
+func (l *Logger) Info(ctx context.Context, v ...interface{}) {
+	l.logWithContext(ctx, l.logger.Info, v...)
 }
 func (l *Logger) Infof(ctx context.Context, format string, v ...interface{}) {
-	if len(l.kv) > 0 {
-		ctx = context.WithValue(ctx, "kv", l.kv)
-	}
-	defer func() {
-		l.kv = nil
-		p.Put(l)
-	}()
-	l.logger.Infof(ctx, format, v...)
+	l.logfWithContext(ctx, l.logger.Infof, format, v...)
 }
 func (l *Logger) Error(ctx context.Context, v ...interface{}) {
-	if len(l.kv) > 0 {
-		ctx = context.WithValue(ctx, "kv", l.kv)
-	}
-	defer func() {
-		l.kv = nil
-		p.Put(l)
-	}()
-	l.logger.Error(ctx, v...)
+	l.logWithContext(ctx, l.logger.Error, v...)
 }
 func (l *Logger) Errorf(ctx context.Context, format string, v ...interface{}) {
-	if len(l.kv) > 0 {
-		ctx = context.WithValue(ctx, "kv", l.kv)
-	}
-	defer func() {
-		l.kv = nil
-		p.Put(l)
-	}()
-	l.logger.Errorf(ctx, format, v)
+	l.logfWithContext(ctx, l.logger.Errorf, format, v...)
 }
 func (l *Logger) Panic(ctx context.Context, v ...interface{}) {
-	if len(l.kv) > 0 {
-		ctx = context.WithValue(ctx, "kv", l.kv)
-	}
-	defer p.Put(l)
-	l.logger.Panic(ctx, v...)
+	l.logWithContext(ctx, l.logger.Panic, v...)
 }
 func (l *Logger) Panicf(ctx context.Context, format string, v ...interface{}) {
-	if len(l.kv) > 0 {
-		ctx = context.WithValue(ctx, "kv", l.kv)
-	}
-	defer func() {
-		l.kv = nil
-		p.Put(l)
-	}()
-	l.logger.Panicf(ctx, format, v)
+	l.logfWithContext(ctx, l.logger.Panicf, format, v...)
 }
 func (l *Logger) Warn(ctx context.Context, v ...interface{}) {
-	if len(l.kv) > 0 {
-		ctx = context.WithValue(ctx, "kv", l.kv)
-	}
-	defer func() {
-		l.kv = nil
-		p.Put(l)
-	}()
-	l.logger.Warn(ctx, v...)
+	l.logWithContext(ctx, l.logger.Warn, v...)
 }
 func (l *Logger) Warnf(ctx context.Context, format string, v ...interface{}) {
-	if len(l.kv) > 0 {
-		ctx = context.WithValue(ctx, "kv", l.kv)
-	}
-	defer func() {
-		l.kv = nil
-		p.Put(l)
-	}()
-	l.logger.Warnf(ctx, format, v)
+	l.logfWithContext(ctx, l.logger.Warnf, format, v...)
 }
 func (l *Logger) Debug(ctx context.Context, v ...interface{}) {
-	if len(l.kv) > 0 {
-		ctx = context.WithValue(ctx, "kv", l.kv)
-	}
-	defer func() {
-		l.kv = nil
-		p.Put(l)
-	}()
-	l.logger.Debug(ctx, v...)
+	l.logWithContext(ctx, l.logger.Debug, v...)
 }
 func (l *Logger) Debugf(ctx context.Context, format string, v ...interface{}) {
-	if len(l.kv) > 0 {
-		ctx = context.WithValue(ctx, "kv", l.kv)
-	}
-	defer func() {
-		l.kv = nil
-		p.Put(l)
-	}()
-	l.logger.Debugf(ctx, format, v)
+	l.logfWithContext(ctx, l.logger.Debugf, format, v...)
 }
 
+// 关闭 Logger
 func (l *Logger) Close(f func()) {
 	f()
-	//GetLogger().close()
 }
